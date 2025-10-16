2467b68685d9f981462a780eba6515e5b683eff0
René <98562029+RenPen3@users.noreply.github.com>
2025-10-15 20:07:45 -0700

    I added a cards.js file to grab the info for cards section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("Hello Moto!");
+import { cards } from "/js/cards.js";
+// const { image, alt, heading, body, cta } = cards;
+
+const displayCard = cards
+  .map(function (card) {
+    return `
+        <div class="cards-content__box cards-content__box--1">
+          <img src="${card.image}" alt="${card.alt}" />
+          <div class="cards-title">${card.heading}</div>
+          <div class="cards-text">${card.body}</div>
+          <a class="cards-btn" href="#">${card.cta}</a>
+        </div>`;
+  })
+  .join("");
+
+document.querySelector(".cards-content").innerHTML = displayCard;
---
+++ js/cards.js
@@ -0,0 +1,30 @@
+export const cards = [
+  {
+    image: "https://placehold.co/600x400",
+    alt: "Image 1",
+    heading: "Title 1",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    cta: "Button 1",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    alt: "Image 2",
+    heading: "Title 2",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    cta: "Button 2",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    alt: "Image 3",
+    heading: "Title 3",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    cta: "Button 3",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    alt: "Image 4",
+    heading: "Title 4",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    cta: "Button 4",
+  },
+];
