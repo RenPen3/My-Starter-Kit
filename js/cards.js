45da4cf62fa0b47a047f02fc2123743a1ecdfd7f
René <98562029+RenPen3@users.noreply.github.com>
2025-10-15 20:15:57 -0700

    I added Unsplash images to the cards section.

--- js/cards.js
+++ js/cards.js
@@ -1,27 +1,31 @@
 export const cards = [
   {
-    image: "https://placehold.co/600x400",
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
     alt: "Image 1",
     heading: "Title 1",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
     cta: "Button 1",
   },
   {
-    image: "https://placehold.co/600x400",
+    image:
+      "https://plus.unsplash.com/premium_photo-1676496046182-356a6a0ed002?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2076",
     alt: "Image 2",
     heading: "Title 2",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
     cta: "Button 2",
   },
   {
-    image: "https://placehold.co/600x400",
+    image:
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
     alt: "Image 3",
     heading: "Title 3",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
     cta: "Button 3",
   },
   {
-    image: "https://placehold.co/600x400",
+    image:
+      "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
     alt: "Image 4",
     heading: "Title 4",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
